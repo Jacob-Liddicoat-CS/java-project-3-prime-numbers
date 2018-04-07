92d10149c4413645527808df6cda72140cd4c4da
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-07 19:15:36 +0000

    printing off first prime number and declaring 3 variables

---
+++ primeNumbers.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class primeNumbers
+{
+ public static void main(String args[])
+ {
+  int n;
+  int status = 1;
+  int num = 3;
+  System.out.println("First 1000 prime numbers are:");
+  System.out.println(2);
+
