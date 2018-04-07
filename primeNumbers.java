a7e4349fbc4a42fc2e33d841ae5720a5b2d72689
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-04-07 19:21:14 +0000

    Add 2 for statements

--- primeNumbers.java
+++ primeNumbers.java
@@ -9,4 +9,8 @@ public class primeNumbers
   int num = 3;
   System.out.println("First 1000 prime numbers are:");
   System.out.println(2);
-
+  for ( int i = 2 ; i <=1000 ; ) //Declares first prime and where to stop
+  {
+   for ( int j = 2 ; j <= Math.sqrt(num) ; j++ ) // Calculates if it is a prime number
+   {
+    
